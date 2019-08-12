abe36583a6d6543f6c13e0a518b21c738d8cecd9
Alvaro Willams Alejandro Arancibia <aljand321@gmail.com>
2019-08-12 18:56:51 -0400

    se modifico doctores para que se acomode a la nueva tabla que se introdujo pero solo una parte

--- server/controllers/doctores.js
+++ server/controllers/doctores.js
@@ -6,7 +6,7 @@ const{ Turnos } = model
 const{ Especialidades } = model;
 
 const { Consulta_especilaida } = model
-
+ const { horas_of_truno } = model
 
 class Doctor{
     static regist(req, res){
@@ -64,7 +64,7 @@ class Doctor{
         Doctores.findAll({
             where: {id: id},
             include:[
-                {model:Especialidades, attributes:['nombre']}
+                {model:Consulta_especilaida, attributes:['nombre']}
             ]               
             }).then((data) => {
                 res.status(200).json(data);
@@ -73,14 +73,14 @@ class Doctor{
 
     //ruta para actualizar
     static modifyDoctCuaderno(req, res) {
-        Especialidades.findAll({
+        Consulta_especilaida.findAll({
             where: {nombre: req.body.especialidad}
         })
         .then((data) => {
             var id = data[0].id
             console.log(" <<<<<<<<<<<<<<<<<   ",id)
             const {nombre,TDoctor, enfermera } = req.body
-            var id_espcialidad = id;
+            var id_ConsultaEspecialidad = id;
             return Doctores
               .findByPk(req.params.id)
               .then((data) => {
@@ -88,7 +88,7 @@ class Doctor{
                     nombre: nombre || data.nombre,
                     TDoctor: TDoctor || data.TDoctor,
                     enfermera: enfermera || data.enfermera,
-                    id_espcialidad:id_espcialidad || data.id_espcialidad
+                    id_ConsultaEspecialidad:id_ConsultaEspecialidad || data.id_ConsultaEspecialidad
                 })
                 .then(update => {
                   res.status(200).send({
@@ -97,7 +97,7 @@ class Doctor{
                         nombre: nombre || update.nombre,
                         TDoctor: TDoctor || update.TDoctor,
                         enfermera: enfermera || update.enfermera,
-                        id_espcialidad:id_espcialidad || update.id_espcialidad
+                        id_ConsultaEspecialidad:id_ConsultaEspecialidad || update.id_ConsultaEspecialidad
                     }
                   })
                 })
@@ -108,7 +108,7 @@ class Doctor{
       }
 
       //
-      static doctorTurnos(req, res){
+    static doctorTurnos(req, res){
         var id = req.params.id;
         Doctores.findAll({
             where : { id : id },
@@ -122,6 +122,25 @@ class Doctor{
           res.status(200).send(users)
         })
     }
+
+    static docAllData(req, res){
+        var id = req.params.id;
+        Doctores.findAll({
+            where : { id : id },
+          //attributes: [],
+            include: [
+                { model: Fechas, 
+                include:[
+                    { model: Turnos, 
+                    include:[
+                        {model: horas_of_truno}
+                    ]}                
+            ]}]
+        }).then(users => {
+          res.status(200).send(users)
+        })
+    }
+
     
 }
 export default Doctor;
